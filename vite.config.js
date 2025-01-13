1ea6420cc053b64fd1e9fe20bd9f572ca31d4618
RaghavOG <04raghavsingla28@gmail.com>
2025-01-13 21:24:44 +0530

    deployment fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true,  // Enable sourcemaps for production build
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
